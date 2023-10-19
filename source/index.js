25c8ca2bd3dddea2697fcce1a7fd8030e1f887ca
FugitiveSine <henrytimmons2004@gmail.com>
2023-10-19 11:38:45 -0700

    main

---
+++ source/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const {Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
+client.on('ready' , (c) => {
+    console.log(`${c.user.tag} is online`)
+})
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand){
+        return;
+    }
+    if (interaction.commandName === 'hey'){
+        interaction.reply('Hey!');
+
+    }
+    if (interaction.commandName === 'ping'){
+        interaction.reply('pong');
+
+    }
+});
+
+client.on('messageCreate', (message) => {
+
+    if(message.author.bot) {
+        return;
+    }
+})
+
+client.login(process.env.TOKEN);
+
